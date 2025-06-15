08c02e5aaa59bc639f4f2f5e75839d92077250da
pfatemi7 <57317656+pfatemi7@users.noreply.github.com>
2025-06-14 17:28:20 -0700

    Delete client directory

--- client/public/placeholder.js
+++
@@ -1,7 +0,0 @@
-// This is just a placeholder file to ensure the directory exists
-// You should replace these with actual images:
-// - logo.png
-// - hero.png
-// - team/parham.jpg
-// - team/nasim.jpg
-// - team/saeid.jpg 
